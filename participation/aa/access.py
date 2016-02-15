bac628fc117e0200c12c6b6760ca6268fb490272
Renato Fabbri <renato.fabbri@gmail.com>
2016-02-15 21:33:30 -0200

    feito outline do acesso

--- participation/aa/access.py
+++ participation/aa/access.py
@@ -1,12 +1,54 @@
 from participation import DATADIR
-import percolation as P, os
+import percolation as P, social as S, os
 from percolation.rdf import NS, a, po, c
-import _mysql
+import MySQLdb, pymongo, codecs
+def connectMysql():
+    db2=MySQLdb.connect(user="root",passwd="foobar",db="fbdb",use_unicode=True)
+    db= MySQLdb.connect(user="root",passwd="foobar",db="fbdb2",use_unicode=True)
+    db.query("SET NAMES utf8")
+    db.query('SET character_set_connection=utf8')
+    db.query('SET character_set_client=utf8')
+    db.query('SET character_set_results=utf8')
+    db.query("show tables;")
+    res=db.store_result()
+    aa_tables=[res.fetch_row()[0][0] for i in range(res.num_rows())]
+    d={}
+    for tt in aa_tables:
+        db.query("select column_name from information_schema.columns where table_name='%s';"%(tt,))
+        res=db.store_result()
+        d["h"+tt]=[res.fetch_row()[0][0] for i in range(res.num_rows())]
+        db.query("select * from %s;"%(tt,))
+        res=db.store_result()
+        d[tt]=[res.fetch_row()[0] for i in range(res.num_rows())]
+    d2={}
+    for tt in aa_tables:
+        db2.query("select column_name from information_schema.columns where table_name='%s';"%(tt,))
+        res=db2.store_result()
+        d2["h"+tt]=[res.fetch_row()[0][0] for i in range(res.num_rows())]
+        db2.query("select * from %s;"%(tt,))
+        res=db2.store_result()
+        d2[tt]=[res.fetch_row()[0] for i in range(res.num_rows())]
+    return d,d2
 
+def connectMongo():
+    client=pymongo.MongoClient("mongodb://labmacambira:macambira00@ds031948.mongolab.com:31948/aaserver")
+    shouts=client.aaserver.shouts.find({})
+    shouts_=[shout for shout in shouts]
+    return shouts
+
+def parseIrcLog():
+    with codecs.open("../../social/data/irc/labmacambira_lalenia3.txt","rb","iso-8859-1")  as f:
+        logtext=S.irc.log2rdf.textFix(f.read())
+    return logtext
 def parseLegacyFiles(data_dir=DATADIR+"aa/"):
     """Parse legacy files with aa shouts and sessions"""
     # access mysql, access mongo, access irc log from social/
+    mysqldata1,mysqldata2=connectMysql()
+    mongoshouts=connectMongo()
+    ircshouts=parseIrcLog()
     filenames=[]
+    triples=[]
+    snapshots=set()
     for filename in filenames:
         snapshotid="irc-legacy-"+filename.replace("#","")
         snapshoturi=po.TwitterSnapshot+"#"+snapshotid
@@ -23,7 +65,7 @@ def parseLegacyFiles(data_dir=DATADIR+"aa/"):
                  (snapshoturi, po.isEgo, False),
                  (snapshoturi, po.isGroup, True),
                  (snapshoturi, po.isFriendship, False),
-                 (snapshoturi, po.isInteraction, True),
+                 (snapshoturi, po.isInteraction, False),
                  (snapshoturi, po.isPost, True),
                  (snapshoturi, po.humanizedName, name_humanized),
                  (snapshoturi, po.expressedReference, expressed_reference),
@@ -36,7 +78,7 @@ def parseLegacyFiles(data_dir=DATADIR+"aa/"):
                  ]
         snapshots.add(snapshoturi)
     nfiles=len(filenames)
-    nsnapshots=len(snapshots)
+    nsnapshots=3
     P.context("participation_aa","remove")
     platformuri=P.rdf.ic(po.Platform,"AA",context="participation_aa")
     triples+=[
