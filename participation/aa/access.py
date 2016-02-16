0ff51162e1ab1b2d7bde3d8bc735d0705b0f62cb
Renato Fabbri <renato.fabbri@gmail.com>
2016-02-16 09:14:07 -0200

    debugado acesso aos mysqls

--- participation/aa/access.py
+++ participation/aa/access.py
@@ -16,22 +16,25 @@ def connectMysql():
     db2.query('SET character_set_connection=utf8')
     db2.query('SET character_set_client=utf8')
     db2.query('SET character_set_results=utf8')
-#    db2.query("show tables;")
 
     db.query("show tables;")
     res=db.store_result()
     aa_tables=[res.fetch_row()[0][0] for i in range(res.num_rows())]
     d={}
     for tt in aa_tables:
-        db.query("select column_name from information_schema.columns where table_name='%s';"%(tt,))
+        #db.query("select column_name from information_schema.columns where table_name='%s';"%(tt,))
+        db.query("SHOW columns FROM %s;"%(tt,))
         res=db.store_result()
         d["h"+tt]=[res.fetch_row()[0][0] for i in range(res.num_rows())]
         db.query("select * from %s;"%(tt,))
         res=db.store_result()
         d[tt]=[res.fetch_row()[0] for i in range(res.num_rows())]
+    db2.query("show tables;")
+    res=db2.store_result()
+    aa_tables=[res.fetch_row()[0][0] for i in range(res.num_rows())]
     d2={}
     for tt in aa_tables:
-        db2.query("select column_name from information_schema.columns where table_name='%s';"%(tt,))
+        db2.query("SHOW columns FROM %s;"%(tt,))
         res=db2.store_result()
         d2["h"+tt]=[res.fetch_row()[0][0] for i in range(res.num_rows())]
         db2.query("select * from %s;"%(tt,))
@@ -48,6 +51,7 @@ def connectMongo():
 def accessIrcLog():
     with codecs.open("../../social/data/irc/labmacambira_lalenia3.txt","rb","iso-8859-1")  as f:
         logtext=S.irc.log2rdf.textFix(f.read())
+        logtext=P.utils.cleanText(logtext)
     return logtext
 def accessOreShouts():
     g=r.Graph()
@@ -65,11 +69,11 @@ def parseLegacyFiles(mysqldbs=True,mongoshouts=True,irclog=True,oreshouts=True):
     # access mysql, access mongo, access irc log from social/
     c("starting aa access")
     if mysqldbs:
-        mysqlbds=connectMysql(); c("mysql ok") # mysqldata1,mysqldata2
+        mysqldbs=connectMysql(); c("mysql ok") # mysqldata1,mysqldata2
     if mongoshouts:
         mongoshouts=connectMongo(); c("mongo ok")
     if irclog:
-        ircslog=accessIrcLog(); c("irc ok")
+        irclog=accessIrcLog(); c("irc ok")
     if oreshouts:
         oreshouts=accessOreShouts(); c("ore ok")
     return mysqldbs,mongoshouts,irclog,oreshouts
